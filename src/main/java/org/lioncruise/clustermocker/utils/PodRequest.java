55c70896af72c6673fd91ebd59c2df83bed086f6
qinshijun <qinshijun16@otcaix.iscas.ac.cn>
2018-04-09 15:54:00 +0800

    Add resource request and limit to pod

--- src/main/java/org/lioncruise/clustermocker/utils/PodRequest.java
+++ src/main/java/org/lioncruise/clustermocker/utils/PodRequest.java
@@ -1,24 +1,28 @@
 package org.lioncruise.clustermocker.utils;
 
-import io.fabric8.kubernetes.api.model.Container;
-import io.fabric8.kubernetes.api.model.ContainerBuilder;
-import io.fabric8.kubernetes.api.model.PodBuilder;
+import io.fabric8.kubernetes.api.model.*;
 import io.fabric8.kubernetes.client.Config;
 import io.fabric8.kubernetes.client.ConfigBuilder;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
-import io.fabric8.kubernetes.api.model.Pod;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class PodRequest {
     public void createPod() {
         Config config = new ConfigBuilder().withMasterUrl("http://47.104.16.133:8080/").build();
         final KubernetesClient client = new DefaultKubernetesClient(config);
 
+        Map<String, Quantity> requestsMap = new HashMap<String, Quantity>();
+        requestsMap.put("cpu", new Quantity("250m"));
+        requestsMap.put("memory", new Quantity("64Mi"));
+
+        Map<String, Quantity> limitsMap = new HashMap<String, Quantity>();
+        limitsMap.put("cpu", new Quantity("500m"));
+        limitsMap.put("memory", new Quantity("128Mi"));
+
         Container container = new ContainerBuilder().withImage("busybox").
+                withNewResources().withRequests(requestsMap).withLimits(limitsMap).endResources().
                 withCommand("sleep", "3600").withImagePullPolicy("IfNotPresent").withName("busybox").build();
 
         Pod pod = new PodBuilder().
